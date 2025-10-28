1838da5e0500e08d4133f675463e14c48ac26446
fabimessi4v <139477474+fabimessi4v@users.noreply.github.com>
2025-10-28 15:16:58 -0300

    Implementacion de consultas 3,4 en front y actualizacion de sidebar

--- frontend/src/_nav.js
+++ frontend/src/_nav.js
@@ -4,8 +4,28 @@ export default [
     name: 'Dashboard',
     to: '/dashboard',
     icon: 'cil-speedometer',
-    badge: {
-      color: 'primary',
-    },
+    badge: { color: 'primary' },
   },
+  {
+    component: 'CNavItem',
+    name: 'Correlacion espacial',
+    to: '/correlacion-espacial',
+    icon: 'cil-calculator',
+    badge: { color: 'primary' },
+  },
+  {
+    component: 'CNavItem',
+    name: 'Eventos extremos',
+    to: '/deteccion-eventos-extremos',
+    icon: 'cil-search',
+    badge: { color: 'primary' },
+  },
+  {
+    component: 'CNavItem',
+    name: 'Resumen Semanal',
+    to: '/ResumenSemanal',
+    icon: 'cil-chart',
+    badge: { color: 'primary' },
+  }
+
 ]
